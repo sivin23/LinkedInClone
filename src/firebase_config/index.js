1cbf1a0c3d25aef664ba89f4cb6dfa074d4835a4
sivin23 <sivin.piechartdigital@gmail.com>
2023-07-08 20:55:04 +0530

    LinkedIn project initialization

---
+++ src/firebase_config/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSyuZ7-kFDE0Fr5jj0PYS0LLnp_nVdDVI",
+  authDomain: "linkedinclone-efe05.firebaseapp.com",
+  projectId: "linkedinclone-efe05",
+  storageBucket: "linkedinclone-efe05.appspot.com",
+  messagingSenderId: "1006074331523",
+  appId: "1:1006074331523:web:1db4380cc7b1541d2ff6b6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
